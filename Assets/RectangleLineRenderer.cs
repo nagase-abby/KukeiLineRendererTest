ca66a355c6905c582e9a75b0bc64bc49a6d35142
nagase-abby <nagase@abby.co.jp>
2023-10-25 12:51:56 +0900

    2つのLineRendererを使用して平行四辺形を作成

--- Assets/RectangleLineRenderer.cs
+++ Assets/RectangleLineRenderer.cs
@@ -1,45 +1,78 @@
 using UnityEngine;
 
+public enum Direction
+{
+    Right,
+
+    Left,
+}
+
 public class RectangleLineRenderer : MonoBehaviour
 {
     [SerializeField]
     private LineRenderer line = null;
 
+    [SerializeField]
+    private Direction direction;
+
+    [SerializeField, Range(0, 10)]
+    private float distance = 1.41421356237f;
+
+    [SerializeField]
+    private bool isMouse = false;
+
+    [SerializeField]
+    private bool isUpdate = false;
+
     private Vector2 GetFirstVertex => line.GetPosition(0);
 
     private void Start()
     {
-        line.positionCount = 4;
-        line.SetPosition(0, Vector3.zero);
-        line.SetPosition(1, Vector3.zero);
-        line.SetPosition(2, Vector3.zero);
-        line.SetPosition(3, Vector3.zero);
+        line.positionCount = 5;
+        line.loop = true;
+        for (int i = 0; i < line.positionCount; i++)
+        {
+            // line.SetPosition(i, Vector3.zero);
+        }
     }
 
     private void Update()
     {
+        if (!isUpdate)
+            return;
+
         Vector3 mousePosition = Input.mousePosition;
-        mousePosition.z = 10;
+        mousePosition.z = 0;
         Vector2 vertex = Camera.main.ScreenToWorldPoint(mousePosition);
 
-        if (Input.GetMouseButtonDown(0))
+        if (isMouse)
         {
-            SetAllPosition(vertex);
+            line.SetPosition(0, vertex);
+            line.SetPosition(2, vertex);
+            line.SetPosition(4, vertex);
         }
 
-        if (Input.GetMouseButton(0))
-        {
-            line.SetPosition(1, new Vector2(vertex.x, GetFirstVertex.y));
-            line.SetPosition(2, vertex);
-            line.SetPosition(3, new Vector2(GetFirstVertex.x, vertex.y));
-        }
+        Test();
     }
 
-    private void SetAllPosition(Vector2 vertex)
+    private void Test()
     {
-        for (int i = 0; i < line.positionCount; i++)
-        {
-            line.SetPosition(i, vertex);
-        }
+        float y1 = GetFirstVertex.y + Mathf.Sin(GetAngle1(direction) * Mathf.Deg2Rad) * distance;
+        float x1 = GetFirstVertex.x + Mathf.Cos(GetAngle1(direction) * Mathf.Deg2Rad) * distance;
+        line.SetPosition(1, new Vector2(x1, y1));
+
+        float y3 = GetFirstVertex.y + Mathf.Sin(GetAngle3(direction) * Mathf.Deg2Rad) * distance;
+        float x3 = GetFirstVertex.x + Mathf.Cos(GetAngle3(direction) * Mathf.Deg2Rad) * distance;
+        line.SetPosition(3, new Vector2(x3, y3));
+    }
+
+    private float GetAngle1(Direction direction)
+    {
+        return direction == Direction.Right ? 25 : 155;
+    }
+
+    private float GetAngle3(Direction direction)
+    {
+        return direction == Direction.Right ? 335 : 205;
     }
 }
